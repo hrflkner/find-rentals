8ac1236022cbc4a02688ec6c36d10a1b38eee64b
hrflkner <hunter.wm.faulkner@gmail.com>
2024-04-07 23:14:13 +1000

    Fetch properties and display

--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -1,13 +1,23 @@
-import Link from 'next/link'
-import React from 'react'
+import PropertyCard from '../../components/PropertyCard'
+import PropertyListing from '@/interfaces/PropertyListing'
+import fetchProperties from '@/utils/fetchProperties'
 
-type Props = {}
-
-export default function PropertiesPage({}: Props) {
+export default async function PropertiesPage() {
+  const properties: PropertyListing[] = await fetchProperties()
+  properties.sort((a, b) => +new Date(b.createdAt) - +new Date(a.createdAt))
   return (
-    <div>
-      <h1 className="text-3xl">Available properties</h1>
-      <Link href="/">Back Home</Link>
-    </div>
+    <section className="px-4 py-6">
+      <div className="container-xl lg:container m-auto px-4 py-6">
+        {!properties.length ? (
+          <p>No properties available!</p>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
+            {properties.map((property) => {
+              return <PropertyCard key={property._id} property={property} />
+            })}
+          </div>
+        )}
+      </div>
+    </section>
   )
 }
---
+++ utils/fetchProperties.ts
@@ -0,0 +1,20 @@
+const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null
+
+export default async function fetchProperties() {
+  try {
+    // Handle when domain is unavailable from prod env
+    if (!apiDomain) {
+      return []
+    }
+    const res = await fetch(`${apiDomain}/properties`)
+
+    if (!res.ok) {
+      throw new Error('Failed to fetch data.')
+    }
+
+    return res.json()
+  } catch (err) {
+    console.error(err)
+    return []
+  }
+}
