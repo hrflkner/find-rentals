40e7f236c05731fe3f9567bce2451acdc71caf3b
hrflkner <hunter.wm.faulkner@gmail.com>
2024-05-05 19:52:23 +1000

    Fix hero absolute image background

--- app/Hero.tsx
+++ app/Hero.tsx
@@ -5,14 +5,14 @@ export default function Hero() {
   return (
     <section className="relative bg-black/25 py-20 md:py-48 lg:py-80 w-full max-h-[60svh] lg:max-h-none md:h-auto aspect-auto overflow-hidden">
       <Image
-        className="absolute top-0 z-[-10] min-w-max w-auto lg:w-full max-h-[75svh] lg:max-h-none lg:h-full mx-auto"
+        className="absolute top-0 min-w-max w-auto lg:w-full max-h-[75svh] lg:max-h-none lg:h-full mx-auto"
         src="/images/sunset_suburb.avif"
         alt="Neighborhood at sunset"
         width={3840}
         height={1596}
         priority
       />
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center">
+      <div className="absolute z-10 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center">
         <div className="text-center">
           <h1 className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl">
             Your Next Home or Rental
